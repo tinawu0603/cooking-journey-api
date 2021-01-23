7548bc37efc60bc9aa0b2c69d1edd07ffc8a2bab
Tina Wu <tina.wu@mark43.com>
2021-01-23 00:31:21 -0500

    add section in main for connecting google app engine to sql server and accessing db-password from secret manager

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module main.go
 go 1.14
 
 require (
+	cloud.google.com/go v0.75.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/ziutek/mymysql v1.5.4
+	google.golang.org/genproto v0.0.0-20210108203827-ffc7fda8c3d7
 )
--- main.go
+++ main.go
@@ -2,14 +2,17 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	_ "github.com/ziutek/mymysql/godrv"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
 
 var Recipes []Recipe
@@ -34,14 +37,44 @@ func handleRequests() {
 }
 
 func main() {
-	// Manually set DB_USERNAME and DB_PASSWORD environment variable
-	db_username := os.Getenv("DB_USERNAME")
-	db_password := os.Getenv("DB_PASSWORD")
-	db_tcphost := "127.0.0.1"
-	db_port := "3306"
-	db_name := "cooking_journey"
+	// Get database password from Google secret manager
+	secretName := "projects/793806054736/secrets/db-password/versions/1"
+	// Create the client
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		fmt.Println("failed to create secretmanager client")
+		panic(err.Error())
+	}
+	// Build the request
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: secretName,
+	}
+	// Call the API
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		fmt.Println("failed to access secret version")
+		panic(err.Error())
+	}
+	// Manually set DB_PASSWORD environment variable
+	// LOCAL DEV
+	// db_password := os.Getenv("DB_PASSWORD")
+	// db_tcphost := "127.0.0.1"
+
+	// GOOGLE CLOUD
+	dbPassword := string(result.Payload.Data)
+	//dbHost := "35.245.37.12"
+	dbUsername := "db-user"
+	//dbPort := "3306"
+	dbName := "cooking_journey"
+	dbConnectionName := "plated-mechanic-302602:us-east4:database-1"
+	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	if !isSet {
+		socketDir = "/cloudsql"
+	}
 	var dbURI string
-	dbURI = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", db_username, db_password, db_tcphost, db_port, db_name)
+	// dbURI = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUsername, dbPassword, dbHost, dbPort, dbName)
+	dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUsername, dbPassword, socketDir, dbConnectionName, dbName)
 
 	db, err = sql.Open("mysql", dbURI)
 	if err != nil {
