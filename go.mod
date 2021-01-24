26f5d9afa0f47f2d3bf3ac97c901a0d8f76c24c7
Tina Wu <tina.wu@mark43.com>
2021-01-24 00:36:16 -0500

    updated main.go to take in dev command line argument

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.14
 require (
 	cloud.google.com/go v0.75.0
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/rs/cors v1.7.0
 	github.com/ziutek/mymysql v1.5.4
 	google.golang.org/genproto v0.0.0-20210108203827-ffc7fda8c3d7
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -15,66 +16,69 @@ import (
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 )
 
-var Recipes []Recipe
 var db *sql.DB
 var err error
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/recipes", getAllRecipesService).Methods("GET")
-	myRouter.HandleFunc("/recipes", createNewRecipeService).Methods("POST")
-	myRouter.HandleFunc("/recipes/{id}", deleteRecipe).Methods("DELETE")
-	myRouter.HandleFunc("/recipes/{id}", getRecipeService).Methods("GET")
-	myRouter.HandleFunc("/recipes/{id}", updateRecipeService).Methods("PUT")
-	// log.Fatal(http.ListenAndServe(":10000", myRouter))
-	http.ListenAndServe(":8080", myRouter)
+	myRouter.HandleFunc("/api/recipes", getAllRecipesService).Methods("GET", "OPTIONS")
+	myRouter.HandleFunc("/api/recipes", createNewRecipeService).Methods("POST", "OPTIONS")
+	myRouter.HandleFunc("/api/recipes/{id}", deleteRecipe).Methods("DELETE", "OPTIONS")
+	myRouter.HandleFunc("/api/recipes/{id}", getRecipeService).Methods("GET", "OPTIONS")
+	myRouter.HandleFunc("/api/recipes/{id}", updateRecipeService).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
-	// Get database password from Google secret manager
-	secretName := "projects/793806054736/secrets/db-password/versions/1"
-	// Create the client
-	ctx := context.Background()
-	client, err := secretmanager.NewClient(ctx)
-	if err != nil {
-		fmt.Println("failed to create secretmanager client")
-		panic(err.Error())
-	}
-	// Build the request
-	req := &secretmanagerpb.AccessSecretVersionRequest{
-		Name: secretName,
-	}
-	// Call the API
-	result, err := client.AccessSecretVersion(ctx, req)
-	if err != nil {
-		fmt.Println("failed to access secret version")
-		panic(err.Error())
-	}
-	// Manually set DB_PASSWORD environment variable
-	// LOCAL DEV
-	// db_password := os.Getenv("DB_PASSWORD")
-	// db_tcphost := "127.0.0.1"
-
-	// GOOGLE CLOUD
-	dbPassword := string(result.Payload.Data)
-	//dbHost := "35.245.37.12"
-	dbUsername := "db-user"
-	//dbPort := "3306"
-	dbName := "cooking_journey"
-	dbConnectionName := "plated-mechanic-302602:us-east4:database-1"
-	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
-	if !isSet {
-		socketDir = "/cloudsql"
+	var isDev bool
+	if len(os.Args) > 1 {
+		isDev = true
+	} else {
+		isDev = false
 	}
 	var dbURI string
-	// dbURI = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUsername, dbPassword, dbHost, dbPort, dbName)
-	dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUsername, dbPassword, socketDir, dbConnectionName, dbName)
+	if isDev {
+		fmt.Println("YOU ARE RUNNING IN DEV MODE!")
+		// DEV MODE:
+		// Running Google SQL Proxy to connect to database
+		dbUsername := "db-user"
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbHost := "127.0.0.1"
+		dbPort := "3306"
+		dbName := "cooking_journey"
+		dbURI = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUsername, dbPassword, dbHost, dbPort, dbName)
+	} else {
+		// CLOUD MODE:
+		// GAE instance connecting to Google SQL server
+		// Get database password from Google secret manager
+		secretName := "projects/793806054736/secrets/db-password/versions/1"
+		// Create the client
+		ctx := context.Background()
+		client, err := secretmanager.NewClient(ctx)
+		if err != nil {
+			fmt.Println("failed to create secretmanager client")
+			panic(err.Error())
+		}
+		// Build the request
+		req := &secretmanagerpb.AccessSecretVersionRequest{
+			Name: secretName,
+		}
+		// Call the API
+		result, err := client.AccessSecretVersion(ctx, req)
+		if err != nil {
+			fmt.Println("failed to access secret version")
+			panic(err.Error())
+		}
+		socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+		if !isSet {
+			socketDir = "/cloudsql"
+		}
+		dbUsername := "db-user"
+		dbPassword := string(result.Payload.Data)
+		dbName := "cooking_journey"
+		dbConnectionName := "plated-mechanic-302602:us-east4:database-1"
+		dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUsername, dbPassword, socketDir, dbConnectionName, dbName)
+	}
 
 	db, err = sql.Open("mysql", dbURI)
 	if err != nil {
